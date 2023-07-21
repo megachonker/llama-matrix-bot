f137b6add8ab82bfbf018f48ab9a8ce353e86e82
megachonker <>
2023-07-22 01:34:04 +0200

    feat: send message

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # llama-matrix-bot
+- [x] receive message
+- [x] send message
+- [x] fix doublON MEssage
+- [ ] crypto
+- [ ] pipe program
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use matrix_sdk::ruma::events::room::message::SyncRoomMessageEvent;
-use matrix_sdk::{
-    config::SyncSettings,
-    Client,
-};
+use matrix_sdk::room::Room;
+use matrix_sdk::ruma::events::room::message::{SyncRoomMessageEvent,RoomMessageEventContent};
+use matrix_sdk::{config::SyncSettings, Client};
 use serde::Deserialize;
 use std::fs::File;
 use std::io::Read;
@@ -25,29 +23,39 @@ fn read_conf() -> ConfigData {
     return serde_yaml::from_str(&config_content).expect("Failed to parse YAML.");
 }
 
-async fn login(conf:ConfigData) -> anyhow::Result<Client> {
+async fn login(conf: ConfigData) -> anyhow::Result<Client> {
     let client = Client::builder()
         .homeserver_url(conf.homeserver_url)
         .build()
         .await?;
 
     client
-        .login_username(&conf.username,&conf.password)
-        .initial_device_display_name("getting started bot").send().await?;
+        .login_username(&conf.username, &conf.password)
+        .initial_device_display_name("getting started bot")
+        .send()
+        .await?;
 
-    println!("logged in as {}",conf.username);
+    println!("logged in as {}", conf.username);
     return Ok(client);
 }
 
-    
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let client = login(read_conf()).await?;
-    client.add_event_handler(|ev: SyncRoomMessageEvent| async move {
+    client.add_event_handler(|ev: SyncRoomMessageEvent, room: Room| async move {
         match &ev.as_original().unwrap().content.msgtype {
-            matrix_sdk::ruma::events::room::message::MessageType::Text(m) => println!("{}",m.body),
+            matrix_sdk::ruma::events::room::message::MessageType::Text(m) => {
+                println!("{}", m.body);
+                let fuck = RoomMessageEventContent::text_plain("MEur MEru MEur");
+                match room {
+                    Room::Joined(raclure) => {raclure.send(fuck, None).await.unwrap();},
+                    _ => println!("LA FE%% CE A F"),
+                }
+            }
             _ => println!("fuckyou"),
         }
+        // room
+        //ned answer fuck yourtself
     });
     client.sync(SyncSettings::default()).await?; // this essentially loops until we kill the bot
     Ok(())
