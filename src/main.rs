6f2d9752c5064e7a53e66094cf72cbe3e00af932
megachonker <>
2023-07-21 23:05:37 +0200

    init: wake up bot an listen message

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use matrix_sdk::ruma::events::room::message::SyncRoomMessageEvent;
+use matrix_sdk::{
+    config::SyncSettings,
+    Client,
+};
+use serde::Deserialize;
+use std::fs::File;
+use std::io::Read;
+
+#[derive(Debug, Deserialize)]
+struct ConfigData {
+    username: String,
+    password: String,
+    homeserver_url: String,
+}
+
+fn read_conf() -> ConfigData {
+    let mut config_file = File::open("config.yaml").expect("Failed to open configuration file.");
+    let mut config_content = String::new();
+
+    config_file
+        .read_to_string(&mut config_content)
+        .expect("Failed to read configuration file.");
+
+    return serde_yaml::from_str(&config_content).expect("Failed to parse YAML.");
+}
+
+async fn login(conf:ConfigData) -> anyhow::Result<Client> {
+    let client = Client::builder()
+        .homeserver_url(conf.homeserver_url)
+        .build()
+        .await?;
+
+    client
+        .login_username(&conf.username,&conf.password)
+        .initial_device_display_name("getting started bot").send().await?;
+
+    println!("logged in as {}",conf.username);
+    return Ok(client);
+}
+
+    
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let client = login(read_conf()).await?;
+    client.add_event_handler(|ev: SyncRoomMessageEvent| async move {
+        match &ev.as_original().unwrap().content.msgtype {
+            matrix_sdk::ruma::events::room::message::MessageType::Text(m) => println!("{}",m.body),
+            _ => println!("fuckyou"),
+        }
+    });
+    client.sync(SyncSettings::default()).await?; // this essentially loops until we kill the bot
+    Ok(())
+}
