9c5a45affd0065a51038c45130fd32502c83cb9e
megachonker <>
2023-07-22 23:14:05 +0200

    feat: lunching args set name of llama for ex

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use matrix_sdk::room::Room;
 use matrix_sdk::ruma::events::room::message::{RoomMessageEventContent, SyncRoomMessageEvent};
-use matrix_sdk::ruma::serde::duration;
 use matrix_sdk::{config::SyncSettings, Client};
 use serde::Deserialize;
 use std::fs::File;
 use std::io::{Read, Write};
-use std::{thread, time};
-use std::process::{Child, Command, Stdio};
+use std::process::{Command, Stdio};
 use std::sync::{Arc, Mutex};
+use std::{thread, time};
 
 #[derive(Debug, Deserialize)]
 struct ConfigData {
@@ -44,50 +43,28 @@ async fn login(conf: ConfigData) -> anyhow::Result<Client> {
     return Ok(client);
 }
 
-// fn llama(input : &String,path:String) -> Vec<u8>{
-//     let azer = Command::new("cat -n")
-//         .stdin(Stdio::piped())
-//         .stdout(Stdio::piped())        .spawn()
-//         .expect("ls command failed to start");
-//     // let output = Command::new(format!("{}/main", path))
-//     //     .arg("--model")
-//     //     .arg(format!("{}/models/30B/ggml-model-f16-ggjt.bin", path))
-//     //     .arg("--prompt")
-//     //     .arg(input)
-//     //     .output()
-//     //     .expect("thats suck")
-//         // .stdout;
-//     return  output;
-// }
-
-// fn llama(input: String, enfant: Arc<Mutex<Child>>) -> Vec<u8> {
-//     let mut process_guard = enfant.lock().unwrap();
-
-    
-
-    
-//     stdin
-//     .write_all(input.as_bytes())
-//     .expect("Failed to write to stdin");
-//     let mut buffer:[u8;10]= [0;10];
-//     // stdout.read(&mut buffer).expect("error read bufferer");
-//     return buffer.to_vec();
-// }
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let mut llama_process =
-        Command::new("./prog")
-            .stdin(Stdio::piped())
-            .stdout(Stdio::piped())
-            .spawn()
-            .expect("cat command failed to start");
+    let configuration_var = read_conf();
 
-    let stdin = Arc::new(Mutex::new(llama_process.stdin.take().expect("Failed to open stdin")));
-    let stdout = Arc::new(Mutex::new(llama_process.stdout.take().expect("Failed to open stdout")));
+    let mut llama_process = Command::new("/bin/bash")
+        .arg(format!("{}/examples/chat-30B.sh", configuration_var.path))
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .env("USER_NAME", "frank")
+        .env("AI_NAME", "javis")
+        .env("N_THREAD", "3")
+        .env("N_PREDICTS", "2048")
+        .spawn()
+        .expect("Failed to lunch LLama");
 
-    let poiu = read_conf();
-    let client = login(poiu).await?;
+    let stdin = Arc::new(Mutex::new(
+        llama_process.stdin.take().expect("Failed to open stdin"),
+    ));
+    let stdout = Arc::new(Mutex::new(
+        llama_process.stdout.take().expect("Failed to open stdout"),
+    ));
+    let client = login(configuration_var).await?;
 
     client.add_event_handler({
         let stdin = stdin.clone();
@@ -102,14 +79,18 @@ async fn main() -> anyhow::Result<()> {
                 }
                 match &ev.as_original().unwrap().content.msgtype {
                     matrix_sdk::ruma::events::room::message::MessageType::Text(m) => {
-                        let mut buf:[u8;100]= [0;100];
+                        let mut buf = String::new();
 
                         stdin.lock().unwrap().write(m.body.as_bytes()).unwrap();
-                        println!("SEND:{}",m.body);
+                        println!("SEND:{}", m.body);
 
-                        let sizer = stdout.lock().unwrap().read(&mut buf).unwrap();
-                        let answer = String::from_utf8(buf.to_vec()).unwrap();
-                        println!("READ: size: {sizer} buf:{:?}",answer);
+                        let sizer = stdout.lock().unwrap().read_to_string(&mut buf).unwrap();
+                        let answer = buf
+                            .as_str()
+                            .lines()
+                            .last()
+                            .expect("last line imposible to get");
+                        println!("READ: size: {sizer} buf:{}", answer);
                         let fuck = RoomMessageEventContent::text_plain(answer); // prépare le message
                         match room {
                             Room::Joined(raclure) => {
